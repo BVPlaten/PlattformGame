d68d39ad6a91628f56450bd2455e606844427a1f
Bernd von Platen <bernd.von.platen@gmail.com>
2020-12-27 16:41:42 +0100

    Timer hinzugefügt

--- szenen/start/RichTextLabel.gd
+++ szenen/start/RichTextLabel.gd
@@ -4,18 +4,24 @@ extends RichTextLabel
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var timer = Timer.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+func _init():
+	add_child(timer)
+	timer.wait_time = 2
+	timer.connect("timeout",self,"_on_timer_timeout")
+	
 
 func _on_TextureButton_pressed():
-	get_node("..").cnt
-	text = "Wurde gedrückt"
+	# get_node("..").cnt
+	text = "Wurde gedrückt"	
+	timer.start()
+
+
+func _on_timer_timeout():
+	text = ""
+	
