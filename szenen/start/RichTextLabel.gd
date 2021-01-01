db1c0d13acf69d4a2e1969345e8cef73b4f6e5cb
Bernd von Platen <bernd.von.platen@gmail.com>
2021-01-01 18:04:40 +0100

    PhysikTest hinzugefügt

--- szenen/start/RichTextLabel.gd
+++ szenen/start/RichTextLabel.gd
@@ -1,29 +1,10 @@
 extends RichTextLabel
 
-
-# https://github.com/BVPlaten/PlattformGame
-var timer = Timer.new()
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-func _init():
-	add_child(timer)
-	timer.wait_time = 1
-	timer.connect("timeout",self,"_on_timer_timeout")
-	
-
 func _on_StartBtn_pressed():
-	text = "Starte das Spiel"
-	timer.start()
+	get_tree().change_scene("res://szenen/alpha_centaurie/Alpha_Centauri.tscn")
+
+func _on_PhysikTestBtn_pressed():
+	get_tree().change_scene("res://szenen/PhysikTest/PhysikTest.tscn")
 
 func _on_EndBtn_pressed():
 	pass 
-	
-
-func _on_timer_timeout():
-	text = ""
-	get_tree().change_scene("res://szenen/alpha_centaurie/Alpha_Centauri.tscn")
-
-	
